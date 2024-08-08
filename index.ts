451b9910b8fb12db4222905a3a38a326f61a4354
Mostafa Mohammadzadeh <mostafa.mdzh@gmail.com>
2024-08-08 16:58:29 +0330

    Revert "[Hot Fix] Exports (#12)"
    This reverts commit 5ec0d36a760aa8428115aa466ae6c3720b42d3c6.

--- index.ts
+++ index.ts
@@ -1,5 +1,3 @@
-export * from './src/host/app-admin-app-loader-main'
-export * from './src/host/app-experience-instant-booking-form-bottom'
-export * from './src/client/sesami-admin-app-loader-main'
-export * from './src/client/sesami-experience-instant-booking-form-bottom'
+export * from './src/host'
+export * from './src/client'
 export * from './src/types'
