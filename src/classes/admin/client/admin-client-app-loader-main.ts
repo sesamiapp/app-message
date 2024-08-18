151799e1002540b968d750936c7e523562839f11
Mostafa Mohammadzadeh <mostafa.mdzh@gmail.com>
2024-08-18 16:02:55 +0330

    Update admin-client-app-loader-main.ts

--- src/classes/admin/client/admin-client-app-loader-main.ts
+++ src/classes/admin/client/admin-client-app-loader-main.ts
@@ -1,5 +1,5 @@
 import { AdminClientBase } from './admin-client-base'
-import { getInit, navBack, showNotification } from '../../../methods/sesami'
+import { getInit, navBack, showNotification } from '../../../methods/client'
 import { initListener } from '../../../helpers'
 
 export class AdminClientAppLoaderMain extends AdminClientBase{
