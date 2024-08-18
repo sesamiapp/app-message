4c6f6eaf1a4ad288d9bf52620b0d66a6e201b9df
Mostafa Mohammadzadeh <mostafa.mdzh@gmail.com>
2024-08-18 16:35:34 +0330

    Update experience-client-instant-booking-form.ts

--- src/classes/experience/client/experience-client-instant-booking-form.ts
+++ src/classes/experience/client/experience-client-instant-booking-form.ts
@@ -1,13 +1,15 @@
 import { ExperienceClientBase } from './experience-client-base'
 import { getInit, acceptNext, onNext, rejectNext } from '../../../methods/client'
-import { initListener } from '../../../helpers'
+import { getUrlParam, initListener } from '../../../helpers'
 
 export class ExperienceClientInstantBookingForm extends ExperienceClientBase{
 
     static init = async () => {
         initListener()
-        const payload: any = await getInit()
+        const id = getUrlParam('id') ?? ''
+        const payload: any = await getInit(id)
         return new ExperienceClientInstantBookingForm({
+            id,
             sessionId: payload.sessionId,
             shopId: payload.shopId,
             productId: payload.productId,
