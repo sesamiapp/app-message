7b674e2436f84ccde859f1306750421f6dd9f365
Mostafa Mohammadzadeh <mostafa.mdzh@gmail.com>
2024-08-18 16:35:28 +0330

    Update admin-client-app-loader-main.ts

--- src/classes/admin/client/admin-client-app-loader-main.ts
+++ src/classes/admin/client/admin-client-app-loader-main.ts
@@ -1,13 +1,15 @@
 import { AdminClientBase } from './admin-client-base'
 import { getInit, navBack, showNotification } from '../../../methods/client'
-import { initListener } from '../../../helpers'
+import { getUrlParam, initListener } from '../../../helpers'
 
 export class AdminClientAppLoaderMain extends AdminClientBase{
     
     static init = async () => {
         initListener()
-        const payload: any = await getInit()
+        const id = getUrlParam('id') ?? ''
+        const payload: any = await getInit(id)
         return new AdminClientAppLoaderMain({
+            id,
             shopId: payload.shopId,
             locale: payload.locale
         })
