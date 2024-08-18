4f6e95d667056b7d9190fb772cbc92945f6434c0
Mostafa Mohammadzadeh <mostafa.mdzh@gmail.com>
2024-08-18 16:02:58 +0330

    Update admin-host-app-loader-main.ts

--- src/classes/admin/host/admin-host-app-loader-main.ts
+++ src/classes/admin/host/admin-host-app-loader-main.ts
@@ -1,5 +1,5 @@
 import { AdminHostBase, AdminHostBaseProps } from './admin-host-base'
-import { onBack, onNotification } from '../../../methods/app'
+import { onBack, onNotification } from '../../../methods/host'
 import { NotificationType } from '../../../types/notification.type'
 
 export class AdminHostAppLoaderMain extends AdminHostBase {
