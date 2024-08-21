cd5b7972e4845ed1b009f6490974be3595a0b8bb
Mostafa Mohammadzadeh <mostafa.mdzh@gmail.com>
2024-08-21 18:27:08 +0330

    Update webpack.config.cjs

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -42,28 +42,3 @@ const nodeConfig = {
   }
 }
 module.exports = [nodeConfig]
-
-// const path = require('path');
-
-// module.exports = {
-//   mode: 'development',
-//   devtool: 'inline-source-map',
-//   entry: {
-//     main: './src/index.ts',
-//   },
-//   output: {
-//     path: path.resolve(__dirname, './build'),
-//     filename: 'index.js',
-//   },
-//   resolve: {
-//     extensions: ['.ts', '.js'],
-//   },
-//   module: {
-//     rules: [
-//       { 
-//         test: /\.tsx?$/,
-//         loader: 'ts-loader'
-//       }
-//     ]
-//   }
-// }
