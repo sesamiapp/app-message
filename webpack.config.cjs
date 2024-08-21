34c6913920069c3aa5db3d849a8d7f83d450e3ae
Mostafa Mohammadzadeh <mostafa.mdzh@gmail.com>
2024-08-21 18:23:14 +0330

    53

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,24 +1,69 @@
-const path = require('path');
+const path = require('path')
 
-module.exports = {
-  mode: "development",
-  devtool: "inline-source-map",
-  entry: {
-    main: "./src/index.ts",
+const nodeConfig = {
+  stats: {
+    errorDetails: true
   },
-  output: {
-    path: path.resolve(__dirname, './build'),
-    filename: 'index.js',
-  },
-  resolve: {
-    extensions: [".ts", ".js"],
-  },
+  mode: 'development',
   module: {
     rules: [
-      { 
+      {
         test: /\.tsx?$/,
-        loader: "ts-loader"
+        use: {
+          loader: 'ts-loader'
+        }
       }
     ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    extensionAlias: {
+      '.js' : ['.ts' , '.js' ],
+      '.jsx': ['.tsx', '.jsx'],
+      '.mjs': ['.mts', '.mjs']
+    }
+  },
+  experiments: {
+    outputModule: true
+  },
+  entry: {
+    node: './src/index.ts'
+  },
+  target: 'node',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname) + '/build',
+    chunkLoading: 'import',
+    chunkFormat: 'module',
+    library: {
+      type: 'module'
+    },
+    globalObject: 'this'
   }
-};
+}
+module.exports = [nodeConfig]
+
+// const path = require('path');
+
+// module.exports = {
+//   mode: 'development',
+//   devtool: 'inline-source-map',
+//   entry: {
+//     main: './src/index.ts',
+//   },
+//   output: {
+//     path: path.resolve(__dirname, './build'),
+//     filename: 'index.js',
+//   },
+//   resolve: {
+//     extensions: ['.ts', '.js'],
+//   },
+//   module: {
+//     rules: [
+//       { 
+//         test: /\.tsx?$/,
+//         loader: 'ts-loader'
+//       }
+//     ]
+//   }
+// }
