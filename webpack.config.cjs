62876e21185ae30e05e4e8ab69396937c519448d
Mostafa Mohammadzadeh <mostafa.mdzh@gmail.com>
2024-08-21 13:34:12 +0330

    48

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,25 +1,75 @@
-const path = require('path');
-// import path from 'path'
+const path = require("path")
 
-module.exports = {
+const commonConfig = {
+  stats: {
+    errorDetails: true
+  },
   mode: "development",
-  devtool: "inline-source-map",
-  entry: {
-    main: "./src/index.ts",
-  },
-  output: {
-    path: path.resolve(__dirname, './build'),
-    filename: 'index.js',
-  },
-  resolve: {
-    extensions: [".ts", ".js"],
-  },
   module: {
     rules: [
-      { 
+      {
         test: /\.tsx?$/,
-        loader: "ts-loader"
+        use: {
+          loader: "ts-loader"
+        }
       }
     ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+    extensionAlias: {
+      ".js": [".ts", ".js"],
+      ".jsx": [".tsx", ".jsx"],
+      ".mjs": [".mts", ".mjs"]
+    }
   }
-};
+}
+
+const browserConfig = {
+  ...commonConfig,
+  experiments: {
+    outputModule: true
+  },
+  entry: { browser: "./src/index.ts" },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: "ts-loader"
+        }
+      }
+    ]
+  },
+  output: {
+    filename: "browser.js",
+    path: path.resolve(__dirname) + "/lib",
+    library: {
+      type: "module"
+    },
+    globalObject: "this"
+  }
+}
+
+const nodeConfig = {
+  ...commonConfig,
+  experiments: {
+    outputModule: true
+  },
+  entry: {
+    node: "./src/index.ts"
+  },
+  target: "node",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname) + "/build",
+    chunkLoading: "import",
+    chunkFormat: "module",
+    library: {
+      type: "module"
+    },
+    globalObject: "this"
+  }
+}
+// module.exports = [browserConfig, nodeConfig]
+module.exports = [nodeConfig]
