04a4f11d77bf04bcbf3441d8918d1c233fef8cc9
Mostafa Mohammadzadeh <mostafa.mdzh@gmail.com>
2024-09-30 07:01:20 +0330

    Update init.client.ts

--- src/methods/client/init.client.ts
+++ src/methods/client/init.client.ts
@@ -5,7 +5,7 @@ export const getInit = (id: string) => {
     const data: Message = {
         action: getEvent(id, Action.INIT)
     }
-    window.top?.postMessage(data)
+    window.top?.postMessage(data, '*')
     return new Promise(resolve => {
         window.addEventListener(getEvent(id, Action.INIT), (e: any) => {
             resolve(e.detail.event.data.payload)
