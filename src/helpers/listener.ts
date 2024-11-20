aa1a00a6168c85d0d3e53a6b56744d1a2e1f6a9d
Mostafa Mohammadzadeh <mostafa.mdzh@gmail.com>
2024-11-20 11:13:52 +0330

    Revert "Revert "ccc""
    This reverts commit d68d63be32060fb19f7b738296660bdc8c870d3c.

--- src/helpers/listener.ts
+++ src/helpers/listener.ts
@@ -1,7 +1,7 @@
 export const initListener = () => {
     if(!window.onmessage){
         window.onmessage = (event: MessageEvent<any>) => {
-            console.info(event.data.action, event)
+            // console.info(event.data.action, event)
             window.dispatchEvent(new CustomEvent(event.data.action, {
                 detail: {
                     event
