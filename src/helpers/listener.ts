7b27f1a8b195f58dc09bfab15bf202ce798e1085
Mostafa Mohammadzadeh <mostafa.mdzh@gmail.com>
2024-12-03 20:00:13 +0330

    75

--- src/helpers/listener.ts
+++ src/helpers/listener.ts
@@ -1,7 +1,7 @@
 export const initListener = () => {
     if(!window.onmessage){
         window.onmessage = (event: MessageEvent<any>) => {
-            // console.info(event.data.action, event)
+            console.info('1.0.75', event.data.action, event)
             window.dispatchEvent(new CustomEvent(event.data.action, {
                 detail: {
                     event
